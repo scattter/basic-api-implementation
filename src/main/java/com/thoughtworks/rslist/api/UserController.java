2bf04ec91188e03bc3e929ec175a43ca093a6778
scattter <413365742@qq.com>
2020-08-06 19:01:01 +0800

    jpa-1

---
+++ src/main/java/com/thoughtworks/rslist/api/UserController.java
@@ -0,0 +1,54 @@
+package com.thoughtworks.rslist.api;
+
+
+import com.thoughtworks.rslist.domain.User;
+import com.thoughtworks.rslist.entity.UserEntity;
+import com.thoughtworks.rslist.exception.CommenError;
+import com.thoughtworks.rslist.exception.InvalidRequestParamException;
+import com.thoughtworks.rslist.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+public class UserController {
+    public static List<User> users = new ArrayList<>();
+    public static List<String> userNames = new ArrayList<>();
+
+    @Autowired
+    UserRepository userRepository;
+
+    @PostMapping("/user")
+    public ResponseEntity register(@RequestBody(required = false) @Valid User user) {
+        UserEntity userEntity = UserEntity.builder()
+                .name(user.getName())
+                .gender(user.getGender())
+                .age(user.getAge())
+                .email(user.getEmail())
+                .phone(user.getPhone())
+                .vote(user.getVote())
+                .build();
+        userRepository.save(userEntity);
+        return ResponseEntity.status(HttpStatus.OK).header("index", "ok").build();
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getUsers() {
+        return ResponseEntity.ok(users);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity exceptionHandler(MethodArgumentNotValidException error) {
+        CommenError commenError = new CommenError();
+        commenError.setError("invalid user");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(commenError);
+    }
+}
